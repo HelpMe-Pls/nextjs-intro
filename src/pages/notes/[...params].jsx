0439bef067edf2750a35348345b00689aae8f08c
Random Guy <leekhoi5319990@gmail.com>
2022-02-27 13:19:31 +0700

    Programmatic routing

--- src/pages/notes/[...params].jsx
+++ src/pages/notes/[...params].jsx
@@ -2,18 +2,24 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const Page = () => {
-    const router = useRouter();
-    
-    // {...} represents all the random routes that will be redirected to THIS page
-    // e.g. notes/some/random/route/6/9/69
-    const { params } = router.query 
-    
-    return (
-        <h1>
-            Note {params}
-        </h1>
-    )
+	const router = useRouter()
+
+	// {...} represents all the random routes that will be redirected to THIS page
+	// e.g. notes/some/random/route/6/9/69
+	const { params } = router.query
+	const id = 2
+
+	return (
+		<>
+			<h1>Note {params}</h1>
+
+			<button onClick={() => router.push('/')}>Go Home</button>
+
+			<button onClick={() => router.push('/user/[id]', `/user/${id}`)}>
+				Dashboard
+			</button>
+		</>
+	)
 }
-    
 
 export default Page
--- src/pages/notes/[id].jsx
+++ src/pages/notes/[id].jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 const Page = () => {
 	const router = useRouter()
@@ -10,6 +11,7 @@ const Page = () => {
 	return (
 		<>
 			<h1>Note: {id}</h1>
+			Back to{' '}
 			<Link href="/notes">
 				<a>Notes</a>
 			</Link>
