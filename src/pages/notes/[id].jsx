b8f3ef90397729547d16a52edd48c1cf1cf20f1f
Random Guy <leekhoi5319990@gmail.com>
2022-02-27 13:09:52 +0700

    Client-side routing

--- src/pages/notes/[id].jsx
+++ src/pages/notes/[id].jsx
@@ -2,17 +2,19 @@ import React from 'react'
 import { useRouter } from 'next/router'
 
 const Page = () => {
-    const router = useRouter();
-    
-    // {id} is whatever in the brackets of the file name
-    const { id } = router.query 
-    
-    return (
-        <h1>
-            Note {id}
-        </h1>
-    )
+	const router = useRouter()
+
+	// {id} is whatever in the brackets of the file name
+	const { id } = router.query
+
+	return (
+		<>
+			<h1>Note: {id}</h1>
+			<Link href="/notes">
+				<a>Notes</a>
+			</Link>
+		</>
+	)
 }
-    
 
 export default Page
--- src/pages/notes/index.js
+++ src/pages/notes/index.js
@@ -1,3 +1,28 @@
 import React from 'react'
+import Link from 'next/link'
 
-export default () => <h1>Notes Index Page</h1>
+export default () => {
+	const notes = new Array(15)
+		.fill(1)
+		.map((ele, i) => ({ id: i, title: `Note: ${i}` }))
+
+	return (
+		<div>
+			<h1>Notes</h1>
+
+			{notes.map((note) => (
+				<div>
+					<Link
+						key={note.id}
+						href="/notes/[id]"
+						as={`/notes/${note.id}`}
+					>
+						<a>
+							<strong>{note.title}</strong>
+						</a>
+					</Link>
+				</div>
+			))}
+		</div>
+	)
+}
